0f7d10019163e4de6b46482d4b081cdf69d5893c
Gagan B <gaganb982006@gmail.com>
2026-07-31 18:28:44 +0530

    Create 013 Function With Return.java

---
+++ Practice problems/Java/Basic/013 Function With Return.java
@@ -0,0 +1,21 @@
+/*
+Given an integer n, You need to use the return keyword to return twice the value of n.
+
+Examples:
+
+Input: n = 2
+Output: 4
+Explanation: 2 * 2 = 4
+Input: n = 4
+Output: 8
+Explanation: 2 * 4 = 8
+Constraints:
+1 ≤ n ≤ 10
+  */
+
+class Solution {
+    int returnValueFunction(int n) {
+        // code here
+        return 2 * n;
+    }
+}
