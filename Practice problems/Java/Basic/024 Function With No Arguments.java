cb4517578321dd516b8e5fcc730290efe602f270
Gagan B <gaganb982006@gmail.com>
2026-08-10 22:56:10 +0530

    Create 024 Function With No Arguments.java

---
+++ Practice problems/Java/Basic/024 Function With No Arguments.java
@@ -0,0 +1,18 @@
+/*
+This problem has no input. You need to write the function helloFunction that prints Hello.
+
+Examples: 
+
+Input: None
+Output: Hello
+Explanation: There is no input. The function prints Hello everytime.
+  */
+
+// Write the helloFunction below to print "Hello"
+
+class Solution {
+    // code here
+    public static void helloFunction(){
+        System.out.print("Hello");
+    }
+}
